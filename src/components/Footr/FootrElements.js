6b2eb6c955c3eba95c8c8b431cd2a0d3b861ed67
stanleyifenna <64693095+stanleyifenna@users.noreply.github.com>
2021-11-27 14:13:02 +0100

    Add files via upload

---
+++ src/components/Footr/FootrElements.js
@@ -0,0 +1,133 @@
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+ 
+export const FooterContainer =styled.footer`
+  background-color: #FAFAFA;
+  padding: 40px 0;
+  width: 100%;
+`
+
+export const FooterWrap =styled.div`
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;     
+`
+
+export const FooterLinksContainer =styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 820px) {
+      padding-top: 32px   
+
+  }  
+`
+
+
+
+
+
+
+{/*
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+ 
+export const FooterContainer =styled.footer`
+  background-color: #FAFAFA;
+`
+
+export const FooterWrap =styled.div`
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;     
+`
+
+export const FooterLinksContainer =styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 820px) {
+      padding-top: 32px   
+
+  }  
+`
+
+export const FooterLinksWrapper =styled.div`
+  display: flex;
+
+  @media screen and (max-width: 820px) {
+  flex-direction: column;
+  
+  }  
+`
+
+export const FooterLinkItems =styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  text-align: left;
+  margin: 16px 32px;  
+  padding: 0px 24px; 
+  width: 160px;
+  box-sizing: border-box;
+  
+  @media screen and (max-width: 420px) {
+    margin: 0;
+    padding: 10px;
+    width: 100%;    
+    }  
+
+    @media screen and (max-width: 820px) {
+      margin: 16px 52px;  
+      padding: 0px 24px;  
+      }  
+
+`
+
+export const FooterLine =styled.h1`
+border: 1px solid red;
+`
+
+export const FooterLinkTitle =styled.h1`
+margin-bottom: 16px;
+color: #000000;
+font-size: 17px;
+line-height: 24px;
+font-weight: 500;
+`
+
+
+export const FooterLink =styled(Link)`
+  font-size: 16px;
+  line-height: 23.5px;
+  text-decoration: none;
+  font-style: regular;
+  font-weight: 400;
+  color: #565656; 
+  margin-bottom: 0.5rem;
+  &:hover {
+     color: #1a71ff;
+  text-decoration: none;
+  transition: 0.3s ease-out;  
+  }
+`
+
+export const SocialMedia = styled.section`
+  max-width: 1000px;
+  display: flex;
+
+`
+*/}
+
+
+
